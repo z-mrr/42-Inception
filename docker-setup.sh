e2214aeaf1e2ca31a947b02b50a4c282b809ab56
Zé Miguel Moreira <zmdmoreira@gmail.com>
2023-09-22 15:41:44 +0100

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -17,4 +17,5 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker $USER
+newgrp docker
 sudo reboot
